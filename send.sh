9b7edc1b992942be2f22a8d84aaed2bae4c6637a
Diego <diego.celdran@paynopain.com>
2025-01-07 13:35:30 +0100

    test

--- send.sh
+++ send.sh
@@ -24,7 +24,8 @@ esac
 CUSTOM_MESSAGE=""
 
 if [ -n "$3" ]; then
-  CUSTOM_MESSAGE=$(echo "$3" | sed -e ':a;N;$!ba' -e 's/\\/\\\\/g' -e 's/"/\\"/g' -e 's/\n/\\n/g')
+  # CUSTOM_MESSAGE=$(echo "$3" | sed -e ':a;N;$!ba' -e 's/\\/\\\\/g' -e 's/"/\\"/g' -e 's/\n/\\n/g')
+  CUSTOM_MESSAGE=$(echo "$3" | jq -Rsa .)
 fi
 
 shift
